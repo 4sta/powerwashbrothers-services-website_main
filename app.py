970c64ef37ba1a0dba15ce93d8f0b5b8da3376d6
Vladyslav Sysoiev <96187703+4sta@users.noreply.github.com>
2024-06-18 11:32:14 +0000

    ADDED API route and Dynamic data
    - imported jsonify module, have created route.
    - added dynamic data with hard coded "services" list for the demonstration purposes.
    - created new html file  "services"  to store a dynamic data for services.

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template   #from moduel importing Flask class
+from flask import Flask, render_template, jsonify  #from moduel importing Flask class
 
 app = Flask(__name__)  #creating Flusk App
 
 
-@app.route("/")   
+SERVICES = [{
+    'id': 1,
+    'title': 'Roof Pressure Washing',
+    'description':
+    ' We provide roof pressure washing services for residential and commercial properties.',
+    'price': ' From $250'
+}, {
+    'id': 2,
+    'title': 'Driveway and Pathway Cleaning',
+    'description':
+    ' Our high-pressure cleaning effectively removes dirt, grime, and stains from your driveways and pathways, ensuring a pristine entrance to your home.',
+    'price': ' From $100'
+}, {
+    'id': 3,
+    'title': 'House Walls and Windows Cleaning',
+    'description':
+    ' We provide thorough cleaning for your home’s exterior walls and windows, enhancing your property’s curb appeal and maintaining its condition.',
+    'price': ' From $150'
+}, {
+    'id': 4,
+    'title': 'Lawn Care Service',
+    'description':
+    ' Available only during the summer and autumn seasons, our lawn care service ensures your grass remains healthy and visually appealing, complementing our main pressure washing services.',
+    'price': ' From $100'
+}]
+
+
+@app.route("/")
 def power_wash():
-  return render_template('homepage.html')
+  return render_template('homepage.html', services=SERVICES)
+
+
+@app.route("/api/services")
+def list_servises():
+  return jsonify(SERVICES)  #abruf der webseite in json format (json API)
 
 
 if __name__ == "__main__":
